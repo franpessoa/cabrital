3cb0dbc744cb610ec2a014e50ef42f4979613da0
Francisco Pessoa <franciscomspessoa@gmail.com>
2023-11-19 17:28:02 -0300

    Remove useless enum

--- src/logic/matriz.hpp
+++ src/logic/matriz.hpp
@@ -1,26 +1,19 @@
 #pragma once
 #include <stdint.h>
-#include <spdlog/spdlog.h>
+#include "spdlog/spdlog.h"
 #include "cabrito.hpp"
 #include "rng.hpp"
 
 namespace Matriz
 {
-    enum class MatrizStep
-    {
-        MATRIZ_STEP_PARTO,
-        MATRIZ_STEP_PARTO_DUPLO,
-        MATRIZ_STEP_NADA
-    };
-
     struct Matriz
     {
         uint32_t idade {6};
 
-        std::vector<Cabrito::Cabrito> step(const double_t parto_duplo_frac)
+        std::vector<Cabrito::Cabrito> step(const double_t parto_duplo_frac, const uint32_t tempo_prenhez)
         {   
-            idade++;
-            if (idade % 6 == 0)
+            ++idade;
+            if (idade % tempo_prenhez == 0)
             {
                 return RNG::Parto(parto_duplo_frac);
             } else {
