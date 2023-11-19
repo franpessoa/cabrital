d651294fae8d8f170a647a48c6e6bcf31dbba561
Francisco Pessoa <franciscomspessoa@gmail.com>
2023-11-19 12:57:22 -0300

    Separate structs for easier configuration

--- src/logic/simulation.cpp
+++ src/logic/simulation.cpp
@@ -3,8 +3,9 @@
 #include "rng.hpp"
 #include <vector>
 #include <iterator>
-#include <spdlog/spdlog.h>
+#include "spdlog/spdlog.h"
 #include "fmt/core.h"
+
 SimulationStep Simulation::step()
 {
     saída = 0;
@@ -12,43 +13,43 @@ SimulationStep Simulation::step()
     spdlog::info(fmt::format("Reached step {}", delta_t_meses));
 
     int idx = 0;
-    for (auto& cabrito: cabritos)
+    for (auto& cabrito: entities.cabritos)
     {
         cabrito.idade_meses++;
         int idx = 0;
-        if (cabrito.idade_meses == tempo_abate_meses)
+        if (cabrito.idade_meses == cfg.tempo_abate_meses)
         {
-            if (cabrito.genero == Cabrito::CabritoGenero::FEMEA && matrizes.size() < max_matrizes)
+            if (cabrito.genero == Cabrito::CabritoGenero::FEMEA && entities.matrizes.size() < cfg.max_matrizes)
             {
-                matrizes.push_back(Matriz::Matriz());
+                entities.matrizes.push_back(Matriz::Matriz());
             } else {
                 saída++;
             }
 
-            cabritos.erase(cabritos.begin() + idx);
-        } else if (RNG::CheckAlive(morte_mensal_frac)) {
-            cabritos.erase(cabritos.begin() + idx);
+            entities.cabritos.erase(entities.cabritos.begin() + idx);
+        } else if (RNG::CheckAlive(cfg.morte_mensal_frac)) {
+            entities.cabritos.erase(entities.cabritos.begin() + idx);
         };
 
         idx++;
     };
 
-    for (int i = 0; i < matrizes.size(); i++)
+    for (int i = 0; i < entities.matrizes.size(); i++)
     {
-        Matriz::Matriz& matriz = matrizes[i];
-        auto step = matriz.step(parto_duplo_frac);
-        cabritos.insert(
-            cabritos.end(),
+        Matriz::Matriz& matriz = entities.matrizes[i];
+        auto step = matriz.step(cfg.parto_duplo_frac);
+        entities.cabritos.insert(
+            entities.cabritos.end(),
             std::make_move_iterator(step.begin()),
             std::make_move_iterator(step.end())
         );
-        if (matriz.idade >= tempo_vida_matriz_meses || RNG::CheckAlive(morte_mensal_matrizes_frac))
+        if (matriz.idade >= cfg.tempo_vida_matriz_meses || RNG::CheckAlive(cfg.morte_mensal_matrizes_frac))
         {
-            matrizes.erase(matrizes.begin() + i);
+            entities.matrizes.erase(entities.matrizes.begin() + i);
         }
     }
 
-    SimulationStep step = {.cabritos = cabritos, .matrizes = matrizes, .saída_mensal = saída, .delta_t_meses = delta_t_meses};
+    SimulationStep step = {.cabritos = entities.cabritos, .matrizes = entities.matrizes, .saída_mensal = saída, .delta_t_meses = delta_t_meses};
     steps.push_back(step);
 
     return step;
--- src/logic/simulation.hpp
+++ src/logic/simulation.hpp
@@ -10,15 +10,8 @@ struct SimulationStep
     uint32_t delta_t_meses;
 };
 
-struct Simulation
+struct SimulationConfig
 {
-    std::vector<SimulationStep> steps = {};
-    std::vector<Cabrito::Cabrito> cabritos = {};
-    std::vector<Matriz::Matriz> matrizes = {};
-    uint32_t delta_t_meses = {0};
-
-    uint32_t saída = {0};
-
     const double_t parto_duplo_frac;
     const double_t morte_mensal_frac;
     const uint32_t tempo_prenhez_meses;
@@ -26,6 +19,23 @@ struct Simulation
     const uint32_t max_matrizes;
     const uint32_t tempo_vida_matriz_meses;
     const double_t morte_mensal_matrizes_frac;
-    
+};
+
+struct SimulationEntities
+{
+    std::vector<Cabrito::Cabrito> cabritos = {};
+    std::vector<Matriz::Matriz> matrizes = {};
+};
+
+struct Simulation
+{
+    std::vector<SimulationStep> steps = {};
+
+    uint32_t delta_t_meses = {0};
+
+    uint32_t saída = {0};
+
+    SimulationConfig cfg;
+    SimulationEntities entities;
     SimulationStep step();
 };
